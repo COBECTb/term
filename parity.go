93277e19936af5969a8b0bd935ce365adcd4d76a
Aleksandr Strokov <sovest@MacBook-Pro-Aleksandr.local>
2020-07-22 19:31:49 +0300

    add get paryty

--- parity.go
+++ parity.go
@@ -37,5 +37,23 @@ func (t *Term) SetParity(parity int) error {
 	return termios.Tcsetattr(uintptr(t.fd), termios.TCSANOW, (*syscall.Termios)(&a))
 }
 
+//GetParity - get parity
+func (t *Term) GetParity() (int, error) {
+	var a attr
+	if err := termios.Tcgetattr(uintptr(t.fd), (*syscall.Termios)(&a)); err != nil {
+		return 0, err
+	}
+
+	if a.Cflag&syscall.PARENB > 0 {
+		if a.Cflag&syscall.PARODD > 0 {
+			return ODD, nil
+		} else {
+			return PAR, nil
+		}
+	} else {
+		return parNONE, nil
+	}
+}
+
 //config.c_cflag &= ~(CSIZE | PARENB);
 //config.c_cflag |= CS8;
